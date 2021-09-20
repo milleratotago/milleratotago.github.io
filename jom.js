85d8898401eb00ca475be610bed819f4181372ab
milleratotago <miller@psy.otago.ac.nz>
2021-09-21 07:29:47 +1200

    Victor LRP paper accepted

--- jom.js
+++ jom.js
@@ -87,7 +87,7 @@ function addFooter()
 	var newSPAN = document.createElement('span');
 	newSPAN.setAttribute('class','update');
         /* NewJeff: Change last update, separately for BIB & RIS files in publications.htm */
-	newSPAN.appendChild(document.createTextNode('Last update: 20 January, 2021'));
+	newSPAN.appendChild(document.createTextNode('Last update: 21 September, 2021'));
 	newAddr.appendChild(newSPAN);
 	footerDiv.appendChild(newAddr);
 
