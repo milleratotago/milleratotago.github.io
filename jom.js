9f05be289a4500cae7b773b6d3d07f7a913fc72b
milleratotago <miller@psy.otago.ac.nz>
2021-11-27 06:51:53 +1300

    msmk 2021 published
    mittelstaedtschaffernakmillerkiesel2021

--- jom.js
+++ jom.js
@@ -87,7 +87,7 @@ function addFooter()
 	var newSPAN = document.createElement('span');
 	newSPAN.setAttribute('class','update');
         /* NewJeff: Change last update, separately for BIB & RIS files in publications.htm */
-	newSPAN.appendChild(document.createTextNode('Last update: 30 October, 2021'));
+	newSPAN.appendChild(document.createTextNode('Last update: 27 November, 2021'));
 	newAddr.appendChild(newSPAN);
 	footerDiv.appendChild(newAddr);
 
