e377fac4efc25f2f18fccbefe81e6ecffd5138da
Jeff Miller <miller@psy.otago.ac.nz>
2024-09-25 15:23:07 +1200

    update 2024-09-04
    added voormannmiller and some page numbers

--- jom.js
+++ jom.js
@@ -87,7 +87,7 @@ function addFooter()
 	var newSPAN = document.createElement('span');
 	newSPAN.setAttribute('class','update');
         /* NewJeff: Change last update, separately for BIB & RIS files in publications.htm */
-	newSPAN.appendChild(document.createTextNode('Last update: 2024-08-19'));
+	newSPAN.appendChild(document.createTextNode('Last update: 2024-09-04'));
 	newAddr.appendChild(newSPAN);
 	footerDiv.appendChild(newAddr);
 
